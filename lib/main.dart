7eb9afbe79640ff9a1397a67b0166f78240d26e8
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-03-10 16:57:34 +0800

    feat: setup firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:plms_clz/views/home.dart';
 import 'package:plms_clz/views/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
