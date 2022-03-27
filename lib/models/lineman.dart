dd51ef34d2fc35156458ac94a827f36dc1dda34b
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-03-28 02:16:34 +0800

    feat(lineman): add update password

--- lib/models/lineman.dart
+++ lib/models/lineman.dart
@@ -39,20 +39,17 @@ class Lineman {
       barangay = data['barangay'];
       apiToken = data['token'];
       fcmToken = data['fcmToken'];
-
-      Fluttertoast.showToast(
-        msg: "Logged in!",
-      );
     } else {
       Fluttertoast.showToast(
         msg: data['message'] ?? 'Failed to login',
+        toastLength: Toast.LENGTH_LONG,
       );
     }
 
     return response.statusCode;
   }
 
-  Future<void> updateFcmToken(String token) async {
+  Future<int> updatePassword(String newPassword) async {
     final url = Uri.https(domain, "/api/lineman/" + id.toString());
     final headers = <String, String>{
       HttpHeaders.contentTypeHeader: ContentType.json.toString(),
@@ -63,14 +60,53 @@ class Lineman {
     final response = await http.patch(
       url,
       headers: headers,
-      body: jsonEncode({"fcm_token": token}),
+      body: jsonEncode({"password": newPassword}),
     );
 
+    final data = jsonDecode(response.body);
+
     if (response.statusCode == HttpStatus.ok) {
-      fcmToken = token;
+      password = newPassword;
     } else {
+      final errors = (data['errors']['password'] as List<dynamic>)
+          .map((e) => e.toString()) as List<String>;
       Fluttertoast.showToast(
-        msg: response.body,
+        msg: errors.isNotEmpty
+            ? errors.join('\n')
+            : 'Failed to update password.',
+        toastLength: Toast.LENGTH_LONG,
+      );
+    }
+
+    return response.statusCode;
+  }
+
+  Future<void> updateFcmToken(String newToken) async {
+    final url = Uri.https(domain, "/api/lineman/" + id.toString());
+    final headers = <String, String>{
+      HttpHeaders.contentTypeHeader: ContentType.json.toString(),
+      HttpHeaders.acceptHeader: ContentType.json.toString(),
+      HttpHeaders.authorizationHeader: "Bearer " + apiToken!,
+    };
+
+    final response = await http.patch(
+      url,
+      headers: headers,
+      body: jsonEncode({"fcm_token": newToken}),
+    );
+
+    final data = jsonDecode(response.body);
+
+    if (response.statusCode == HttpStatus.ok) {
+      fcmToken = newToken;
+    } else {
+      final errors = (data['errors']['fcm_token'] as List<dynamic>)
+          .map((e) => e.toString()) as List<String>;
+      Fluttertoast.showToast(
+        msg: errors.isNotEmpty
+            ? errors.join('\n')
+            : 'Failed to update fcm token.',
+        toastLength: Toast.LENGTH_LONG,
       );
     }
   }
