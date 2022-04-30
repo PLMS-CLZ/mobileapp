d3027a212c9789303d4357b70b267f547de9d44c
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-04-30 18:44:23 +0800

    feat(incidentinfo): add format helper

--- lib/models/incidentinfo.dart
+++ lib/models/incidentinfo.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class IncidentInfo {
   int id;
   String title;
@@ -14,12 +16,39 @@ class IncidentInfo {
   });
 
   factory IncidentInfo.fromJson(Map<String, dynamic> json) {
+    int i = 0;
     return IncidentInfo(
       id: json['id'],
-      title: json['title'],
+      title: (json['title'] as String)
+          .split("")
+          .map((e) => i++ == 0 ? e.toUpperCase() : e)
+          .join(""),
       description: json['description'],
       createdAt: json['created_at'],
       updatedAt: json['updated_at'],
     );
+  }
+
+  List<Widget> format() {
+    return [
+      RichText(
+        text: TextSpan(
+          style: const TextStyle(fontSize: 16),
+          children: [
+            TextSpan(
+              text: "$title - ",
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            TextSpan(
+              text: description,
+              style: const TextStyle(color: Colors.white70),
+            ),
+          ],
+        ),
+      ),
+      const SizedBox(height: 10),
+      Text(updatedAt, style: const TextStyle(color: Colors.white60)),
+      const SizedBox(height: 15),
+    ];
   }
 }
