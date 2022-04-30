b46200d72a2b03fb3f3fcaa138f57e26c9c11c8b
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-04-30 18:43:57 +0800

    feat(incidents): add locations attribute, add areasAffected helper

--- lib/models/incident.dart
+++ lib/models/incident.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:plms_clz/models/incidentinfo.dart';
+import 'package:plms_clz/models/location.dart';
 
 class Incident {
   int id;
@@ -6,6 +9,7 @@ class Incident {
   String createdAt;
   String updatedAt;
   List<IncidentInfo> info;
+  List<Location> locations;
 
   Incident({
     required this.id,
@@ -13,17 +17,39 @@ class Incident {
     required this.createdAt,
     required this.updatedAt,
     required this.info,
+    required this.locations,
   });
 
   factory Incident.fromJson(Map<String, dynamic> json) {
     return Incident(
-      id: json['id'],
-      resolved: json['resolved'],
-      createdAt: json['created_at'],
-      updatedAt: json['updated_at'],
-      info: (json['info'] as List<dynamic>)
-          .map((e) => IncidentInfo.fromJson(e))
-          .toList(),
-    );
+        id: json['id'],
+        resolved: json['resolved'],
+        createdAt: json['created_at'],
+        updatedAt: json['updated_at'],
+        info: (json['info'] as List<dynamic>)
+            .map((e) => IncidentInfo.fromJson(e))
+            .toList(),
+        locations: (json['locations'] as List<dynamic>)
+            .map((e) => Location.fromJson(e))
+            .toList());
+  }
+
+  List<Widget> areasAffected() {
+    return locations.map((e) {
+      return RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: "\t\t\t• ${e.city} - ",
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            TextSpan(
+              text: e.barangays.join(", "),
+              style: const TextStyle(color: Colors.white70),
+            )
+          ],
+        ),
+      );
+    }).toList();
   }
 }
