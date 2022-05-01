235737d17c854a55708b964ddb977a371d4cbb74
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-05-02 01:41:45 +0800

    chore(unit): add phone number, createdAt, and updatedAt attributes

--- lib/models/unit.dart
+++ lib/models/unit.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/material.dart';
+
 class Unit {
   int id;
   String status;
   double latitude;
   double longitude;
+  String phoneNumber;
+  String createdAt;
+  String updatedAt;
   String formattedAddress;
 
   Unit({
@@ -10,6 +15,9 @@ class Unit {
     required this.status,
     required this.latitude,
     required this.longitude,
+    required this.phoneNumber,
+    required this.createdAt,
+    required this.updatedAt,
     required this.formattedAddress,
   });
 
@@ -19,6 +27,9 @@ class Unit {
       status: json['status'],
       latitude: double.parse(json['latitude']),
       longitude: double.parse(json['longitude']),
+      phoneNumber: json['phone_number'],
+      createdAt: json['created_at'],
+      updatedAt: json['updated_at'],
       formattedAddress: json['formatted_address'],
     );
   }
