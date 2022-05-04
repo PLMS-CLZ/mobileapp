de66f7bfb8ad1affe2f069d324fdc22efb6c642f
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-05-05 02:01:50 +0800

    chore(constants): add googleApiDomain, googleApiKey

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,3 +3,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 const domain = "plms-clz.herokuapp.com";
 
 const cadizCity = LatLng(10.956592, 123.307354);
+
+const googleApiDomain = "maps.googleapis.com";
+const googleApiKey = "AIzaSyBV-bmZ8qXI2rkhKl-7CjUPGrkWnCaM23g";
